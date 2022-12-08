ad9cbd4a20958692ba681a470b4b7e846302a7e7
rakibul-wdp <rakibul.wdp@gmail.com>
2022-12-08 13:55:18 +0600

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <button class="btn btn-primary">Button</button>
-    </div>
+    <>
+      <Navbar/>
+    </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import SearchIcon from "@mui/icons-material/Search";
+import React from 'react';
+import logo from "../assets/images/logo.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="header-input">
+          <SearchIcon className="search-icon" />
+          <input
+            type="text"
+            placeholder="Search for other favourite groups in ATG"
+          />
+        </div>
+        <p className="header-left">
+          Create Account <span className="header-left-blue">It's Free!</span>
+          <ArrowDropDownIcon />
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
