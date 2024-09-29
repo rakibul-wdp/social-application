c11d7083c396764bdb2b0dd770d01db0636aa672
rakibul-wdp <rakibul.wdp@gmail.com>
2024-09-30 04:05:00 +0600

    create cards component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../css/Card.css";
+import "../css/Common.css";
+import ArticleCard from "./ArticleCard";
+import EducationCard from "./EducationCard";
+import JobCard from "./JobCard";
+import MeetupCard from "./MeetupCard";
+import SearchContent from "./SearchContent";
+
+const Cards = () => {
+  return (
+    <div className="card-main">
+      <div className="card-division">
+        <ArticleCard />
+        <EducationCard />
+        <MeetupCard />
+        <JobCard />
+      </div>
+      <SearchContent />
+    </div>
+  );
+};
+
+export default Cards;
