8606a3de53684c93248afe8574e292fb54cde892
rakibul-wdp <rakibul.wdp@gmail.com>
2025-02-08 11:04:33 +0600

    created search content component

---
+++ src/components/SearchContent.jsx
@@ -0,0 +1,31 @@
+import EditIcon from "@mui/icons-material/Edit";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import React from "react";
+import "../css/Card.css";
+import "../css/Common.css";
+
+const SearchContent = () => {
+  return (
+    <div className="card-left">
+      <div className="card-left-title">
+        <div className="card-left-text">
+          <LocationOnOutlinedIcon />{" "}
+          <span className="location-icon-tect">Noida, India</span>
+        </div>
+        <div>
+          <EditIcon />
+        </div>
+      </div>
+      <div className="card-left-info">
+        <InfoOutlinedIcon />{" "}
+        <span>
+          Your location will help us serve better and extend a personalized
+          experience.
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default SearchContent;
